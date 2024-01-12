c1f30ed3a19be616e4b60026a1abf6fde824cbba
Taha <taha.naseem589@gmail.com>
2024-01-13 01:13:30 +0500

    Fix bug in functionality

---
+++ task5.cpp
@@ -0,0 +1,31 @@
+/*Write a C++ program to implement a class called Circle that has private member variables for radius.
+Include member functions to calculate the circle's area and circumference.*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+class circle
+{
+    double radius, area, circumference;
+    double calculate(double radius)
+    {
+        area = M_PI * radius * radius;
+        circumference = 2 * M_PI * radius;
+        return area, circumference;
+    }
+
+public:
+    double publicMemberFunction(double radius)
+    {
+        calculate(radius);
+        cout << "Area is " << area << "\n";
+        cout << "Circumference is " << circumference;
+    }
+};
+int main()
+{
+    double radius;
+    circle obj;
+    cout << "Enter radius ";
+    cin >> radius;
+    obj.publicMemberFunction(radius);
+}
